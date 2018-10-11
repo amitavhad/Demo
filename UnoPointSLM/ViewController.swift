92422418902e426cf38e2c2ab40254c317f29bab
Amit A <amit@nirajs-macbook-air.local>
2018-10-11 17:22:47 +0530

    Initial Commit

---
+++ UnoPointSLM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnoPointSLM
+//
+//  Created by Amit A on 11/10/18.
+//  Copyright © 2018 Amit A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
